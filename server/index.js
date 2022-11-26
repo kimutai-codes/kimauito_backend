365740ea6e353c043a1412b6f29f2802ef336afb
kimutai-codes <allankimutaikoech@gmail.com>
2022-11-26 10:13:00 +0300

    moving PC

--- server/index.js
+++ server/index.js
@@ -1,25 +1,26 @@
 require("dotenv-safe").config({ silent: true });
-const express = require('express'); //import express
-const bodyParser = require('body-parser');
+const express = require("express"); //import express
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express(); //initialize the server
 app.use(express.json()); //parse to json
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 //router routes
-const usersRoute = require('./routes/users');
-const supplierRoute = require('./routes/suppliers');
+const usersRoute = require("./routes/users");
+const supplierRoute = require("./routes/suppliers");
 //API routes
-app.use('/api/users', usersRoute); //we put the route in /api/users
-app.use('/api/suppliers', supplierRoute);
-app.use('/api/products', require('./routes/products'));
-app.use('/api/supply', require('./routes/supply'));
-app.use('/api/order_details', require('./routes/orderDetails'));
-app.use('/api/orders', require('./routes/orders'));
+app.use("/api/users", usersRoute); //we put the route in /api/users
+app.use("/api/suppliers", supplierRoute);
+app.use("/api/products", require("./routes/products"));
+app.use("/api/supply", require("./routes/supply"));
+app.use("/api/order_details", require("./routes/orderDetails"));
+app.use("/api/orders", require("./routes/orders"));
 
 //port to run app
-app.listen(process.env.PORT || '3000', () => {
-	console.log(
-		`server is up and runing on port: ${process.env.PORT || '3000'}`
-	);
+const port = "5000";
+app.listen(process.env.PORT || port, () => {
+  console.log(`server is up and runing on port: ${process.env.PORT || port}`);
 });
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,67 +1,67 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router(); //create router
-const db = require('../db');
+const db = require("../db");
 
 //get all users
-router.get('/', (req, res, next) => {
-	let sql = `SELECT * FROM users`;
-	db.query(sql, (err, results) => {
-		if (err) console.log('sql error');
-		res.json(results);
-	});
+router.get("/", (req, res, next) => {
+  let sql = `SELECT * FROM users`;
+  db.query(sql, (err, results) => {
+    if (err) console.log("sql error");
+    res.json(results);
+  });
 });
 
 // get one use by user_id
-router.get('/:user_id', (req, res, next) => {
-	let sql = `SELECT * FROM users WHERE user_id = ?`;
-	db.query(sql, [req.params.user_id], (err, results) => {
-		if (err) throw err;
-		res.json(results);
-	});
+router.get("/:user_id", (req, res, next) => {
+  let sql = `SELECT * FROM users WHERE user_id = ?`;
+  db.query(sql, [req.params.user_id], (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
 });
 
 //add user
 //TODO this should be restricted to the admin alone
-router.post('/', (req, res, next) => {
-	let newUser = req.body; //the data will be send through the body
-	let sql = `INSERT INTO users SET ?`;
-	db.query(sql, newUser, (err, results) => {
-		if (err) throw err;
-		res.json(results); //server response
-	});
+router.post("/", (req, res, next) => {
+  let newUser = req.body; //the data will be send through the body
+  let sql = `INSERT INTO users SET ?`;
+  db.query(sql, newUser, (err, results) => {
+    if (err) throw err;
+    res.json(results); //server response
+  });
 });
 
 //update user
 //TODO this should be restricted to the admin alone
-router.put('/:user_id', (req, res) => {
-	let sql =
-		'UPDATE users SET `first_name`=?, `second_name`=?, `user_phone`=?, `user_email`=?, `password`=?, `user_role`=? WHERE user_id=?';
-	db.query(
-		sql,
-		[
-			req.body.first_name,
-			req.body.second_name,
-			req.body.user_phone,
-			req.body.user_email,
-			req.body.password,
-			req.body.user_role,
-			req.params.user_id, //this gotta be ordered as the sql querry above is ordered
-		],
-		(err, results) => {
-			if (err) throw err;
-			res.json(results);
-		}
-	);
+router.put("/:user_id", (req, res) => {
+  let sql =
+    "UPDATE users SET `first_name`=?, `second_name`=?, `user_phone`=?, `user_email`=?, `password`=?, `user_role`=? WHERE user_id=?";
+  db.query(
+    sql,
+    [
+      req.body.first_name,
+      req.body.second_name,
+      req.body.user_phone,
+      req.body.user_email,
+      req.body.password,
+      req.body.user_role,
+      req.params.user_id, //this gotta be ordered as the sql querry above is ordered
+    ],
+    (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    }
+  );
 });
 
 // delete user
 //TODO this should be restricted to the admin alone
-router.delete('/:user_id', (req, res) => {
-	let sql = 'DELETE FROM users WHERE user_id = ?';
-	db.query(sql, [req.params.user_id], (err, results) => {
-		if (err) throw err;
-		res.json(results);
-	});
+router.delete("/:user_id", (req, res) => {
+  let sql = "DELETE FROM users WHERE user_id = ?";
+  db.query(sql, [req.params.user_id], (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
 });
 
 //TODO
